4c6e7c04b3a6c19c0623b927f536c7d3c2b6035c
VenkatramanC <venkatraman.c9@gmail.com>
2016-02-05 15:08:07 +0530

    Views modified for heroku

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,30 +6,30 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-      if @question.save 
+      if @question.save
       redirect_to questions_path
       else
-      render 'new' 
-      end 
+      render 'new'
+      end
   end
 
   def index
    @questions = Question.all
-   
+
   end
 
   def show
-    @question = Question.find(params[:id] )
+    @question = Question.find(params[:id])
     @answers = @question.answers
   end
-  
+
   def edit
   end
 
 
   def delete
   end
-    
+
  private
 
  def question_params
